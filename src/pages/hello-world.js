6b7f40655ed2dc219453a70672c938090414584a
Ahmed El-Daly <aeldaly@gmail.com>
2019-04-26 10:32:19 -0400

    Hello World - Routing not working

---
+++ src/pages/hello-world.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div>
+        <p>HELLO WORLD</p>
+      </div>
+    )
+  }
+}
+
+export default HelloWorld
