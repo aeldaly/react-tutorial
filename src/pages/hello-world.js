acae8214df45f24e5a7dfb76e78d644eb330d847
Liz-Lebedeva <liz.lebedeva@joist.com>
2019-05-03 19:30:30 -0400

    homework week 2: added text input and label to hello-world page

--- src/pages/hello-world.js
+++ src/pages/hello-world.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
 
 class HelloWorld extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      greeting: "Hello, stranger!"
+    }
+  }
+
+  handleChange(event) {
+    let greeting = event.target.value ? "Hello, " + event.target.value + "!" : "Hello, stranger!"
+    this.setState({greeting})
+  }
+
   render() {
     return (
       <div>
-        <p>HELLO WORLD</p>
+        <label>{this.state.greeting}</label>
+        <br/>
+        <input
+          type="text"
+          // value={this.state.greeting}
+          onChange={this.handleChange.bind(this)}/>
       </div>
     )
   }
